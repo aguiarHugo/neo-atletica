fb633ea08ab48bfd7c771c46e509bb5d815598ad
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-29 11:51:28 -0300

    BenefitType and ClassType

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,3 +4,15 @@ export enum SelectedPage {
   Aulas = 'aulas',
   Contato = 'contato'
 }
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
