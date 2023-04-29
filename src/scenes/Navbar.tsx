fce9076de5ae4672d33f07a7e822c027b47b05d3
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-29 09:57:35 -0300

    Styled

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -16,10 +16,10 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
   const flexBetween = 'flex items-center justify-between'
   const [menuToggle, setMenuToggle] = useState<boolean>(false)
   const isAboveMediumScreens = useMediaQuery('(min-width: 1060px')
-  const navbarBackground = isTopOfPage ? '' : 'bg-yellow-400 drop-shadow'
+  const navbarBackground = isTopOfPage ? 'bg-dark-100' : 'bg-yellow-400 drop-shadow'
 
   return (
-    <nav>
+    <nav className='max-h-[70px]'>
       <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-full px-16`}>
           <div className={`${flexBetween} w-full`}>
@@ -65,7 +65,7 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
 
       {/* MOBILE MENU MODAL */}
       {!isAboveMediumScreens && menuToggle && (
-        <div className='fixed w-full z-40 bg-yellow-400 h-full drop-shadow-xl menu-slideFromTop'>
+        <div className='fixed w-full h-full top-0 z-40 bg-yellow-400  drop-shadow-xl menu-slideFromTop'>
           <div className='flex justify-end py-[3.3rem] pr-[4.5rem]'>
             <button 
                 onClick={() => setMenuToggle(!menuToggle)}
@@ -73,7 +73,7 @@ const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
               <XMarkIcon className='h-7 w-7 text-red-400 '/>
             </button>
           </div>
-          <div className='flex flex-col items-center mt-20 gap-12 text-xl'>
+          <div className='flex flex-col items-center gap-12 text-xl'>
               <Link 
                 page='Home'
                 selectedPage={selectedPage}
