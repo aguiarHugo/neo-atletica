dd48b54224586d26739565f1a5af37e443fc8508
aguiarHugo <aguiarhugofx@gmail.com>
2023-05-05 12:54:52 -0300

    Styled

--- src/components/Class.tsx
+++ src/components/Class.tsx
@@ -6,12 +6,12 @@ type Props = {
 
 const Class = ({ name, description, image }: Props) => {
   const overlayStyles = `p-5 absolute z-30 flex
-  h-[300px] md:w-[400px] flex-col items-center justify-center
+  h-[270px] md:h-[300px] w-[360px] md:w-[400px] flex-col items-center justify-center
   whitespace-normal bg-yellow-500 text-center text-white
   opacity-0 transition duration-500 hover:opacity-90`;
 
   return (
-    <li className="scroll relative mx-5 inline-block h-[380px] w-[380px] md:w-[400px]">
+    <li className="scroll relative mx-1 sm:mx-5 inline-block h-[340px] w-[360px] md:w-[400px]">
       <div className={overlayStyles}>
         <p className="text-2xl">{name}</p>
         <p className="mt-5">{description}</p>
